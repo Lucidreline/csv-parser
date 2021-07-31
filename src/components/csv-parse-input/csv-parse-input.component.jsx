30114ce332f66901aa0217ecd8111ce5d2e37dc4
Lucidreline <casta.ma502@gmail.com>
2021-07-31 14:11:19 -0700

    Files are turned into CSV-file arrays when dropped in.

--- src/components/csv-parse-input/csv-parse-input.component.jsx
+++ src/components/csv-parse-input/csv-parse-input.component.jsx
@@ -3,19 +3,30 @@ import React from "react";
 import "./csv-parse-input.styles.scss";
 
 const CsvParseInput = () => {
+  const [highlighted, setHighlighted] = React.useState(false);
+
   return (
     <div>
       <div
-        className="input-box"
+        className={`csv-drop ${highlighted ? "highlight" : ""}`}
+        onDragEnter={() => setHighlighted(true)}
+        onDragLeave={() => setHighlighted(false)}
         onDragOver={(e) => {
           e.preventDefault(); // prevents files form being downloaded when dragged in the browser (needs to be done on onDrop too for it to work)
         }}
         onDrop={(e) => {
           e.preventDefault();
-          console.log(e);
+          console.log(e.dataTransfer.files);
+          // convert from filelist to array
+          Array.from(e.dataTransfer.files)
+            .filter(
+              (file) => file.type === "text/csv" // only adds csv files to the array
+            )
+            .forEach((file) => {});
+          setHighlighted(false);
         }}
       >
-        <span>Drop CSV Here</span>
+        <span className="csv-drop-text">Drop CSV Here</span>
       </div>
     </div>
   );
